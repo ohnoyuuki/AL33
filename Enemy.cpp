6cb2dfd669d64bb9a9e085082195d393190f1858
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-07-13 01:32:29 +0900

    henkou

---
+++ Enemy.cpp
@@ -0,0 +1,15 @@
+#include "Enemy.h"
+
+//初期化
+void Enemy::Initialize(){
+
+}
+
+//更新
+void Enemy::Update(){
+
+}
+// 描画
+void Enemy::Draw(){
+
+}
---
+++ Enemy.h
@@ -0,0 +1,11 @@
+#pragma once
+class Enemy {
+public:
+	// 初期化
+	void Initialize();
+	// 更新
+	void Update();
+	// 描画
+	void Draw();
+
+};
