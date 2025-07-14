7ed27f741f482e24df72c5a9848dc8a60c347072
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-07-14 18:34:14 +0900

    henkou

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,15 +1,27 @@
 #include "Enemy.h"
+#include "MapChipField.h"
+#include "MyMath.h"
+#include <algorithm>
+#include <numbers>
+
 
 //初期化
-void Enemy::Initialize(){
+void Enemy::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, KamataEngine::Vector3& position) {
+
 
 }
 
 //更新
 void Enemy::Update(){
 
+
+
+
+	worldTransform_.matWorld_ = MakeAffineMatrix(worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+
+	worldTransform_.TransferMatrix();
 }
 // 描画
 void Enemy::Draw(){
 
-}
+	model_->Draw(worldTransform_, *camera_); }
--- Enemy.h
+++ Enemy.h
@@ -1,11 +1,19 @@
 #pragma once
+#include "KamataEngine.h"
+
 class Enemy {
 public:
 	// 初期化
-	void Initialize();
+	void Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, KamataEngine::Vector3& position);
 	// 更新
 	void Update();
 	// 描画
 	void Draw();
 
+	private:
+	KamataEngine::WorldTransform worldTransform_;
+
+	KamataEngine::Model* model_ = nullptr;
+
+	KamataEngine::Camera* camera_ = nullptr;
 };
